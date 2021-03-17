f8579bc13c542a9c05455e52e6e0e8b96d0df427
William Johnson <joh15132@byui.edu>
2021-03-16 22:58:11 -0600

    more progress

--- don't even know here.py
+++ don't even know here.py
@@ -14,9 +14,21 @@ def prep_data():
     print("after")
     print(data.loc[[0]])
     
-    data.columns = ["title", "year", "age_group", "imdb", "rotten_tomatoes", "netflix", "hulu", "prime video", "disney+", "director(s)", "runtime"]
+    data.columns = ["title", "year", "age_group", "imdb", "rotten_tomatoes", "netflix", "hulu", "prime_video", "disney_plus", "director", "runtime"]
     print(data.loc[[0]])
     #data = np.array(input)
     #print(data[0,:])
+
+    data.title.fillna("unknown")
+    data.year.fillna("unknown")
+    data.age_group.fillna("unknown")
+    data.imdb.fillna("unknown")
+    data.rotten_tomatoes.fillna("unknown")
+    data.netflix.fillna("unknown")
+    data.hulu.fillna("unknown")
+    data.prime_video.fillna("unknown")
+    data.disney_plus.fillna("unknown")
+    data.director.fillna("unknown")
+    data.runtime.fillna("unknown")
 
 prep_data()
